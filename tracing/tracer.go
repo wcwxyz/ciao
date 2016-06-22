1c3cea694e6110ff1b77db7e2ce30b43ee0a031c
Samuel Ortiz <sameo@linux.intel.com>
2016-06-22 17:18:31 +0200

    tracing: Document exported Tracer structures and APIs
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- tracing/tracer.go
+++ tracing/tracer.go
@@ -22,17 +22,27 @@ import (
 	"github.com/docker/distribution/uuid"
 )
 
+// Component is a tracing identifier for each ciao component.
 type Component string
 
 const (
+	// Anonymous is a special component for anonymous tracing.
+	// Anonymous tracing can only carry span messages but not
+	// component specific payloads.
 	Anonymous Component = "anonymous"
-	SSNTP     Component = "ssntp"
-	Libsnnet  Component = "libsnnet"
+
+	// SSNTP is for tracing SSNTP traffic.
+	SSNTP Component = "ssntp"
+
+	// Libsnnet is for tracing ciao's networking.
+	Libsnnet Component = "libsnnet"
 )
 
 const nullUUID = "00000000-0000-0000-0000-000000000000"
 const spanChannelDepth = 256
 
+// Tracer is a handle to a ciao tracing agent that will collect
+// local spans and send them back to ciao trace collectors.
 type Tracer struct {
 	ssntpUUID uuid.UUID
 	component Component
@@ -41,10 +51,16 @@ type Tracer struct {
 	spanChannel chan Span
 }
 
+// TraceContext is an opaque structure that gets passed to Trace()
+// calls in order to link spans together.
+// If you want to link a span A to span B, you should pass the
+// trace context returned when calling Trace() to create span A to
+// the Trace() call for creating span B.
 type TraceContext struct {
 	parentUUID uuid.UUID
 }
 
+// NewComponentTracer creates a tracer for a specific component.
 func NewComponentTracer(component Component, spanner Spanner, ssntpUUID uuid.UUID) (*Tracer, *TraceContext, error) {
 	rootUUID, err := uuid.Parse(nullUUID)
 	if err != nil {
@@ -67,6 +83,7 @@ func NewComponentTracer(component Component, spanner Spanner, ssntpUUID uuid.UUI
 	return &tracer, &traceContext, nil
 }
 
+// NewTracer creates an anonymous tracer.
 func NewTracer(ssntpUUID uuid.UUID) (*Tracer, *TraceContext, error) {
 	return NewComponentTracer(Anonymous, AnonymousSpanner{}, ssntpUUID)
 }
@@ -81,5 +98,8 @@ func (t *Tracer) spanListener() {
 	}
 }
 
+// Stop will stop a tracer.
+// Spans will no longer be listened for and thus won't make
+// it up to a trace collector.
 func (t *Tracer) Stop() {
 }
